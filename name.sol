4bec08579a768929d1796e8ea4cc99d7118e8381
seniraliev <seniraliev94@gmail.com>
2022-01-19 20:28:24 +0100

    contract to return a name passed as a parameter

---
+++ name.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TestContract{
+
+string public name;
+
+function showName (string memory _name) public returns (string memory){
+     name = _name;
+     return name;
+}
+}
